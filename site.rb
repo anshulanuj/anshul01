dfa37a171f018a91d421d260cd080732fee1c0a0
Anshul <anshul.khandelwal01@gmail.com>
2016-05-19 22:33:58 +0000

    Update index view

--- site.rb
+++ site.rb
@@ -2,13 +2,10 @@ require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
-  "Hello World!
-  <h1> Something here, remember to close it </h1>
-  <h2> This will be a medium size header </h2>
-  <h3> This will we a small size header </h3>
-  <p> And this will be a paragraph </p>"
+  erb :index
 end
 get '/italian' do
    "Ciao!"
 end
+
 
