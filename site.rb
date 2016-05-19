1a3606445f38c01271f3b0c951cea1fa6a32310a
Anshul <anshul.khandelwal01@gmail.com>
2016-05-19 21:48:38 +0000

    Add layout and index view

--- site.rb
+++ site.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
+get '/' do
+  "Hello World!
+  <h1> Something here, remember to close it </h1>
+  <h2> This will be a medium size header </h2>
+  <h3> This will we a small size header </h3>
+  <p> And this will be a paragraph </p>"
+end
 get '/italian' do
    "Ciao!"
- end
+end
+
